cd3a72967880adbf19d8294ffec9d5a25602d00e
Aspen Wilson <aspen.schell@gmail.com>
2023-01-12 15:37:36 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function () {
+    return ['Antonia','Nuru'];
+}
+
+const returnLastTwoDrivers = function () {
+    return ['Amari','Mo'];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier() {
+    return function (fare) {
+        return fare * 5;
+    }
+}
+
+function fareDoubler (fare) {
+    return fare*2;
+}
+
+function fareTripler (fare) {
+    return fare*3;
+}
+
+
+const selectDifferentDrivers = (arrayOfDrivers,returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
+
+
+
+
